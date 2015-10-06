6d58f9b029dc5c4b84dddc200fc765df53a0a0b9
KRademacher <Kevin_Rademacher@hotmail.com>
2015-10-06 11:51:44 +0200

    06-10-2015 Added Add Form
    Added Add Form to the Program

---
+++ Main programma/Barroc-IT_Groep5/Barroc-IT_Groep5/frm_Add.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Barroc_IT_Groep5
+{
+    partial class frm_Add
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Back = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Location = new System.Drawing.Point(270, 350);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
+            this.btn_Add.TabIndex = 0;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            // 
+            // btn_Back
+            // 
+            this.btn_Back.Location = new System.Drawing.Point(735, 350);
+            this.btn_Back.Name = "btn_Back";
+            this.btn_Back.Size = new System.Drawing.Size(75, 23);
+            this.btn_Back.TabIndex = 1;
+            this.btn_Back.Text = "Back";
+            this.btn_Back.UseVisualStyleBackColor = true;
+            // 
+            // frm_Add
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 441);
+            this.Controls.Add(this.btn_Back);
+            this.Controls.Add(this.btn_Add);
+            this.MaximizeBox = false;
+            this.Name = "frm_Add";
+            this.Text = "Add";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Back;
+    }
+}
---
+++ Main programma/Barroc-IT_Groep5/Barroc-IT_Groep5/frm_Add.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Barroc_IT_Groep5
+{
+    public partial class frm_Add : Form
+    {
+        public frm_Add()
+        {
+            InitializeComponent();
+        }
+    }
+}
